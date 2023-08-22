1e6b480805053f50f6e17491665e98ec69351d51
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2023-08-22 19:14:50 -0300

    button config

--- CSCFG/src/Screen.java
+++ CSCFG/src/Screen.java
@@ -1,7 +1,10 @@
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class Screen extends JFrame {
     public Screen(){
+        //Configurations
         setTitle("CFG Config");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -9,10 +12,17 @@ public class Screen extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
         
+        //BG Color
+        
+        getContentPane().setBackground(new Color(11,0,28));
+
+        //Layout
         setLayout(null);
 
         JButton cfg = new JButton("CFG");
-        cfg.setBounds(400,300,100,50);
+        cfg.setBounds(720,10,60,30);
+        cfg.setForeground(new Color(255, 255, 255));
+        cfg.setBackground(new Color(11,0,28));
         add(cfg);
     }
 }
