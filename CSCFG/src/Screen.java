8f435baae9b1ce67a8bc593b149adbd11dc2c2b7
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2023-08-23 20:46:10 -0300

    uptade

--- CSCFG/src/Screen.java
+++ CSCFG/src/Screen.java
@@ -2,70 +2,76 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
 
 import javax.swing.*;
 
 public class Screen extends JFrame {
 
-    String dot = "cl_crosshairdot 1";
-    public Screen(){
-        //Configurations
+    public Screen() {
+        // Configurations
         setTitle("CFG Config");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setResizable(false);
         setLocationRelativeTo(null);
-        
-        //BG Color
-        
-        getContentPane().setBackground(new Color(11,0,28));
 
-        //Layout
+        // BG Color
+
+        getContentPane().setBackground(new Color(11, 0, 28));
+
+        // Layout
         setLayout(null);
 
-        //Botao CFG
-        JButton cfgButton = new JButton("CFG");
+        // CheckBox
+        JCheckBox dotCheckBox = new JCheckBox();
+        dotCheckBox.setText("Ponto na Mira");
+        dotCheckBox.setBounds(200, 100, 110, 20);
 
-        cfgButton.setBounds(720,10,60,30);
-        
-        cfgButton.setForeground(new Color(255, 255, 255));
-        cfgButton.setBackground(new Color(11,0,28));
-        
-        cfgButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-                System.out.println("Funcionou");
-            }
-            
-        });
-        
-
-        add(cfgButton);
-        
-        //CheckBox
-        JCheckBox dotCheckBox = new JCheckBox("Ponto na Mira");
-
-        dotCheckBox.setBounds(200,100,110,20);
-        
         dotCheckBox.setForeground(new Color(255, 255, 255));
-        dotCheckBox.setBackground(new Color(11,0,28));
+        dotCheckBox.setFocusable(false);
+        dotCheckBox.setBackground(new Color(11, 0, 28));
         dotCheckBox.isDisplayable();
+
+        // Action
         dotCheckBox.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-                System.out.println("Tbm Funfa");
+
             }
-            
+
         });
 
         add(dotCheckBox);
+        // Botao CFG
+        JButton cfgButton = new JButton();
+        cfgButton.setText("CFG");
+        cfgButton.setBounds(720, 10, 60, 30);
 
-        
+        cfgButton.setForeground(new Color(255, 255, 255));
+        cfgButton.setBackground(new Color(11, 0, 28));
 
+        add(cfgButton);
+
+        cfgButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==cfgButton){
+                    System.out.println(dotCheckBox.isSelected());
+                    if(dotCheckBox.isSelected()){
+                        System.out.println("Ta selecionado");
+                    }else{
+                        System.out.println("ta nao o");
+                    }
+                }
+            }
+
+        });
     }
+
 }
